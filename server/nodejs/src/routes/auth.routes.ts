8b26e80ea91a3502038647693e93537094bb3f1a
Isacco-B <git@isaccobertoli.com>
2024-07-24 16:04:38 +0200

    complete auth functionality

--- server/nodejs/src/routes/auth.routes.ts
+++ server/nodejs/src/routes/auth.routes.ts
@@ -1,13 +1,21 @@
 import express, { Router } from "express";
 import {
+  login,
+  logout,
   newPassword,
   newVerification,
+  refresh,
   register,
+  reset,
 } from "../controllers/auth.controller";
 
 const router: Router = express.Router();
 
 router.post("/sign-up", register);
+router.post("/sign-in", login);
+router.post("/logout", logout);
+router.get("/refresh", refresh);
+router.post("/reset", reset);
 router.post("/new-password/:token", newPassword);
 router.post("/new-verification/:token", newVerification);
 
