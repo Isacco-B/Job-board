1847411d79989bb5f4b9393cffae182a27c4e9c9
Isacco-B <git@isaccobertoli.com>
2024-07-24 16:04:13 +0200

    add shema for reset password

--- server/nodejs/src/schemas/index.ts
+++ server/nodejs/src/schemas/index.ts
@@ -33,5 +33,29 @@ export const RegisterSchema = z
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
-export const ResetSchema = z.object({});
-export const NewPasswordSchema = z.object({});
+
+export const ResetSchema = z.object({
+  email: z
+    .string()
+    .email({ message: "Invalid email" })
+    .min(1, { message: "Email is required" }),
+});
+
+export const NewPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(1, { message: "Password is required" })
+      .max(24, { message: "Maximum 24 characters" })
+      .regex(passwordRegex, {
+        message:
+          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+      }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Confirm password is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
