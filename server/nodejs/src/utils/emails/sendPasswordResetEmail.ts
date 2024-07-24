80357f272e89cf53af53275f4cfdbc7cc7f5ab35
Isacco-B <git@isaccobertoli.com>
2024-07-24 16:04:57 +0200

    change email link format

--- server/nodejs/src/utils/emails/sendPasswordResetEmail.ts
+++ server/nodejs/src/utils/emails/sendPasswordResetEmail.ts
@@ -1,7 +1,7 @@
 import { sendEmail } from "../helpers";
 
 export const sendPasswordResetEmail = async (to: string, token: string) => {
-  const resetLink = `http://localhost:4000/api/auth/new-password?token=${token}`;
+  const resetLink = `http://localhost:4000/api/auth/new-password/${token}`;
   const text = `Please click on the following link to reset your password: ${resetLink}`;
   const html = `<p>Click <a href="${resetLink}">here</a> to reset your password</p>`;
 
--- server/nodejs/src/utils/emails/sendVerificationEmail.ts
+++ server/nodejs/src/utils/emails/sendVerificationEmail.ts
@@ -1,7 +1,7 @@
 import { sendEmail } from "../helpers";
 
 export const sendVerificationEmail = async (to: string, token: string) => {
-  const confirmLink = `http://localhost:4000/api/auth/new-verification?token=${token}`;
+  const confirmLink = `http://localhost:4000/api/auth/new-verification/${token}`;
   const text = `Please click on the following link to reset your password: ${confirmLink}`;
   const html = `<p>Click <a href="${confirmLink}">here</a> to verify your email</p>`;
 
