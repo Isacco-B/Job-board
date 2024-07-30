090b2cf02c5305ed348634f4bb60160805c28bc6
Isacco-B <git@isaccobertoli.com>
2024-07-30 15:33:03 +0200

    complete auth serializer

--- server/python/app/serializers/auth_serializer.py
+++ server/python/app/serializers/auth_serializer.py
@@ -2,17 +2,33 @@ from flask_restx import fields
 from ..utils.extensions import api
 
 
-login_model = api.model("Login", {"email": fields.String, "password": fields.String})
+login_model = api.model(
+    "Login",
+    {
+        "email": fields.String(required=True, description="User email"),
+        "password": fields.String(required=True, description="User password"),
+    },
+)
 register_model = api.model(
     "Register",
     {
-        "name": fields.String,
-        "email": fields.String,
-        "password": fields.String,
-        "confirm_password": fields.String,
+        "name": fields.String(required=True, description="User name"),
+        "email": fields.String(required=True, description="User email"),
+        "password": fields.String(required=True, description="User password"),
+        "confirm_password": fields.String(
+            required=True, description="Confirm user password"
+        ),
     },
 )
-reset_model = api.model("Reset", {"email": fields.String})
+reset_model = api.model(
+    "Reset", {"email": fields.String(required=True, description="User email")}
+)
 new_password_model = api.model(
-    "NewPassword", {"password": fields.String, "confirm_password": fields.String}
+    "NewPassword",
+    {
+        "password": fields.String(required=True, description="User password"),
+        "confirm_password": fields.String(
+            required=True, description="Confirm user password"
+        ),
+    },
 )
