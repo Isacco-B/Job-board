d21087a8e73f2dda29e03c646b30a604d0005513
Isacco-B <git@isaccobertoli.com>
2024-07-30 15:28:56 +0200

    complete auth schema

--- server/python/app/schemas/auth_schema.py
+++ server/python/app/schemas/auth_schema.py
@@ -1,16 +1,80 @@
-from flask_restx import Resource, reqparse
-from ..utils.extensions import api
+from marshmallow import Schema, fields, ValidationError, validate, validates_schema
 
-register_schema = reqparse.RequestParser()
-register_schema.add_argument(
-    "name", type=str, required=True, help="Name is required!"
-)
-register_schema.add_argument(
-    "email", type=str, required=True, help="Email is required!"
-)
-register_schema.add_argument(
-    "password", type=str, required=True, help="Password is required!"
-)
-register_schema.add_argument(
-    "confirm_password", type=str, required=True, help="Confirm Password is required!"
-)
+password_regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$"
+
+
+class LoginSchema(Schema):
+    email = fields.Email(
+        required=True,
+        validate=validate.Length(min=1, error="Email cannot be empty"),
+        error_messages={"required": "Email is required", "invalid": "Invalid email"},
+    )
+    password = fields.Str(
+        required=True,
+        validate=validate.Length(min=1, error="Password cannot be empty"),
+        error_messages={"required": "Password is required"},
+    )
+
+
+class RegisterSchema(Schema):
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(min=1, error="Name cannot be empty"),
+        error_messages={"required": "Name is required"},
+    )
+    email = fields.Email(
+        required=True,
+        validate=validate.Length(min=1, error="Email cannot be empty"),
+        error_messages={"required": "Email is required", "invalid": "Invalid email"},
+    )
+    password = fields.Str(
+        required=True,
+        validate=[
+            validate.Regexp(
+                password_regex,
+                error="Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and between 8 and 24 characters",
+            ),
+        ],
+        error_messages={"required": "Password is required"},
+    )
+    confirm_password = fields.Str(
+        required=True, error_messages={"required": "Confirm password is required"}
+    )
+
+    @validates_schema
+    def validate_confirm_password(self, data, **kwargs):
+        if data.get("password") != data.get("confirm_password"):
+            raise ValidationError(
+                "Passwords do not match", field_names=["confirm_password"]
+            )
+
+
+class ResetSchema(Schema):
+    email = fields.Email(
+        required=True,
+        validate=validate.Length(min=1, error="Email cannot be empty"),
+        error_messages={"required": "Email is required", "invalid": "Invalid email"},
+    )
+
+
+class NewPasswordSchema(Schema):
+    password = fields.Str(
+        required=True,
+        validate=[
+            validate.Regexp(
+                password_regex,
+                error="Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and between 8 and 24 characters",
+            ),
+        ],
+        error_messages={"required": "Password is required"},
+    )
+    confirm_password = fields.Str(
+        required=True, error_messages={"required": "Confirm password is required"}
+    )
+
+    @validates_schema
+    def validate_confirm_password(self, data, **kwargs):
+        if data.get("password") != data.get("confirm_password"):
+            raise ValidationError(
+                "Passwords do not match", field_names=["confirm_password"]
+            )
